cc72eb3fe02352af08d485e8dda0c2ffab3c408a
Vini <vinicius.polimeno@aluno.senai.br>
2025-02-20 08:29:01 -0300

    feat: subindo atividade API

---
+++ Context/Filmes_Context.cs
@@ -0,0 +1,29 @@
+﻿using api_filmes_senai.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_filmes_senai.Context
+{
+    public class Filmes_Context : DbContext
+    {
+        public Filmes_Context()
+        {    
+        }
+        public Filmes_Context(DbContextOptions<Filmes_Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+   
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server = NOTE33-S28\\SQLEXPRESS; Database = filmes; User id = sa; Pwd = Senai@134; trustServerCertificate=true;");
+
+            }
+            
+        }
+
+    }
+}
---
+++ Domains/Genero.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    [Table("Genero")]
+    public class Genero
+    {
+        [Key]
+        public Guid IdGenero { get; set; }
+        [Column(TypeName = "VARCHAR(30)")]
+        [Required(ErrorMessage = "Nome do Gênero e obrigatorio")]
+        public string? Nome { get; set; }
+    }
+}
